f2ea14f53001615137d6a49dabba7ff102cf8f5d
Moisés Guerra <moa.guerra99@gmail.com>
2025-10-31 20:27:05 -0500

    IS1MFGM-0: COMMIT INICIAL ESTRUCTURA PROYECTO

---
+++ src/main/java/co/edu/unbosque/admntarjetas/AdmnTarjetasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.admntarjetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmnTarjetasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmnTarjetasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/unbosque/admntarjetas/AdmnTarjetasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.admntarjetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmnTarjetasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
