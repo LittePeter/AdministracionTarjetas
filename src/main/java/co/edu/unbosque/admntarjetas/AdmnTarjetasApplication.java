d07d61cda50e216d870b77db7704625f049bd8a2
Moisés Guerra <moa.guerra99@gmail.com>
2025-11-16 18:50:28 -0500

    Cors added

--- src/main/java/co/edu/unbosque/admntarjetas/AdmnTarjetasApplication.java
+++ src/main/java/co/edu/unbosque/admntarjetas/AdmnTarjetasApplication.java
@@ -2,6 +2,9 @@ package co.edu.unbosque.admntarjetas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdmnTarjetasApplication {
@@ -9,5 +12,16 @@ public class AdmnTarjetasApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdmnTarjetasApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
